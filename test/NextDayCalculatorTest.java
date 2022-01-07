b83c2454f65fdfe10e7a6dbdee29ee98adeace7d
VuVanHao <vuvanhaohc123@gmail.com>
2022-01-07 22:12:41 +0700

    add test 31/1/2021

--- test/NextDayCalculatorTest.java
+++ test/NextDayCalculatorTest.java
@@ -11,4 +11,14 @@ class NextDayCalculatorTest {
         String expected = "2/1/2021";
         assertEquals(expected,result);
     }
+
+    @org.junit.jupiter.api.Test
+    void TestDay31Month1Year2021(){
+        int dayTest = 31;
+        int monthTest = 1;
+        int yearTest = 2021;
+        String result = NextDayCalculator.getNextDay(dayTest,monthTest,yearTest);
+        String expected = "1/2/2021";
+        assertEquals(expected,result);
+    }
 }
