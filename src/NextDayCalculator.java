7f8b9bc0a586f8107cf6527a40767a075c368bec
VuVanHao <vuvanhaohc123@gmail.com>
2022-01-07 22:22:13 +0700

    test pass 30/4/2021

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -4,6 +4,25 @@ public class NextDayCalculator {
     public static final int StartOfMonth = 1;
     public static String getNextDay(int day, int month,int year)
     {
+        int EndOfMonth = 0;
+        switch (month){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                EndOfMonth = 31;
+                break;
+            case 2:
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                EndOfMonth = 30;
+                break;
+        }
         if(day == EndOfMonth)
         {
             day = StartOfMonth;
